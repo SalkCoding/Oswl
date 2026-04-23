d061ea9da35e9e730890612c0697f1d6f68e1c36
SalkCoding <josangbeom1104@gmail.com>
2026-04-23 23:28:04 +0900

    Init commit

---
+++ src/main/java/com/salkcoding/oswl/OswlApplication.java
@@ -0,0 +1,13 @@
+package com.salkcoding.oswl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OswlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OswlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salkcoding/oswl/OswlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salkcoding.oswl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OswlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
